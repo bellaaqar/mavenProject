a083aaa93c2d0e1437da08af3c9dca3dfb78502c
bellaaqaryassine <yassine3iga@gmail.com>
2021-01-06 23:23:24 +0100

    2 Mains

---
+++ src/main/java/com/Main2.java
@@ -0,0 +1,16 @@
+package com;
+
+import com.traitement.Work;
+
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+
+public class Main2 {
+    public static void main(String[] args ) throws PropertyVetoException, IOException {
+        Work work = new Work();
+        work.traitement();
+
+
+
+    }
+}
